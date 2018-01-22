66aec9ac922238c13dafc2c6b09107ff04777ddb
821484451 <821484451@qq.com>
2018-01-22 13:57:54 +0800

    修复登录后无法直接读取localstorage问题

--- vue-project/src/component/registerSucess/registerSucess.vue
+++ vue-project/src/component/registerSucess/registerSucess.vue
@@ -6,22 +6,31 @@
             <p>你已经成为iGola的一员</p>
         </section>
         <footer class="reg_sf">
-            <div>好的</div>
+            <router-link to="/login">好的</router-link>
         </footer>    
     </div>
 </template>
 <script type="text/javascript">
-    // // 把尺寸放大N倍（N是window.devicePixelRatio）
-    // var wd = document.documentElement.clientWidth*window.devicePixelRatio/10;
-    // //物理像素*设备像素比=真实像素
-    // document.getElementsByTagName("html")[0].style.fontSize = wd + "px";
-    // // 把屏幕的倍率缩小到N分之一（N是window.devicePixelRatio）
-    // var scale = 1/window.devicePixelRatio;
-    // var mstr = 'initial-scale='+ scale +', maximum-scale='+ scale +', minimum-scale='+ scale +', user-scalable=no';
-    // document.getElementById("vp").content = mstr;
+   
 
 
     export default{
+        mounted:function(){
+                var wd = document.documentElement.clientWidth*window.devicePixelRatio/10;
+                    document.getElementsByTagName("html")[0].style.fontSize = wd + "px";
+                    var scale = 1/window.devicePixelRatio;
+                    var mstr = 'initial-scale='+ scale +', maximum-scale='+ scale +', minimum-scale='+ scale +', user-scalable=no';
+                    document.getElementById("vp").content = mstr;
+                  
+        },
+        beforeRouteLeave(to,from,next){
+                //console.log(333)
+                document.getElementById("vp").content = ''
+                document.getElementById("vp").content = 'width=device-width, initial-scale=1.0'
+                document.getElementsByTagName("html")[0].style.fontSize = 10+"px"
+                // console.log(333)
+                next()
+        }
 
     }
 
@@ -33,7 +42,7 @@
     .reg_sh{flex:1;font-size:0.453333rem;line-height:1.106667rem;color:white;text-align:center;}
     .reg_ss{flex:1;font-size:0.453333rem;text-align:center;}
     .reg_ss>p:nth-child(1){height:2.08rem;font-size:2rem;color:white;}
-    .reg_ss>p:nth-child(2){font-size:0.5rem;color:white;line-height:2.453333rem;}
+    .reg_ss>p:nth-child(2){font-size:0.5rem;color:#fff;line-height:2.453333rem;}
     .reg_sf{flex:1;}
     .reg_sf>a{display:block;width:5.706667rem;height:2.08rem;background:#0096FF;border-radius:1.04rem;margin:0 auto;color:#fff;text-align:center;line-height:2.08rem;font-size:0.746667rem;}
     
