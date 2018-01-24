a6f40df39d1b887548817f91d87fb3cbc297169d
william <290035807@qq.com>
2018-01-24 20:08:30 +0800

    整合

--- vue-project/src/component/publicSearch/publicSearch.vue
+++ vue-project/src/component/publicSearch/publicSearch.vue
@@ -1,25 +1,27 @@
 <template>
 	<div class="searchLike">
-		<div id="publicSearch">
+		<div id="publicSearch" class="clearfix">
+			<div class="searchLeft"></div>
 			<div class="search fl">
 				<span class="iconfont icon-sousuo fl"></span>
 				<input class="fl" v-model="value" placeholder="搜索酒店名、关键字、地标" @focus="popUp" @blur="popDown" v-on:input="searchHotel">
 			</div>
-			<span @click='returnHistory1' class="fr">取消</span>	
+			<span @click='returnHistory1' class="fr can">取消</span>	
 		</div>
-		<p v-if="watchEnter">{{value}}</p>
-		<p>
-			最近搜索: 
-		</p>
-		<p>
-			<span v-for = "(obj,index) in historyHotel" :key="obj.index" v-if="index<2">{{obj.hName}}</span>
-		</p>
-		<ul>
-			<li v-for="(obj,index) in searchList" v-heightLight="{index:index}" @click='sendNew(obj.id)' class="res">
-				{{obj.hotelName}}
-			</li>
-		</ul>
-		<div class="mask" v-if="searchSwitch"></div>
+		<div class="searchBottom">
+			<p v-if="watchEnter">{{value}}</p>
+			<p>
+				最近搜索: 
+			</p>
+			<p>
+				<span v-for = "(obj,index) in historyHotel" :key="obj.index" v-if="index<2">{{obj.hName}}</span>
+			</p>
+			<ul>
+				<li v-for="(obj,index) in searchList" v-heightLight="{index:index}" @click='sendNew(obj.id)' class="res">
+					{{obj.hotelName}}
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -93,20 +95,6 @@
 			        this.historyHotel = arrAll;
 				}
 			}
-			
-		},
-		mounted:function(){
-			var wd = document.documentElement.clientWidth*window.devicePixelRatio/10;
-            document.getElementsByTagName("html")[0].style.fontSize = wd + "px";
-            var scale = 1/window.devicePixelRatio;
-            var mstr = 'initial-scale='+ scale +', maximum-scale='+ scale +', minimum-scale='+ scale +', user-scalable=no';
-            document.getElementById("vp").content = mstr;	
-		},
-		beforeRouteLeave(to,from,next){
-			document.getElementById("vp").content = ''
-			document.getElementById("vp").content = 'width=device-width, initial-scale=1.0'
-			document.getElementsByTagName("html")[0].style.fontSize = 10+"px"
-			next()
 		}
 	}
 </script>
