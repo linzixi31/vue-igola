4484629378eb17d2763a26399f9fd198b6712867
linzixi31 <912379914@qq.com>
2018-01-29 16:31:25 +0800

    解决路径build的问题

--- vue-project/src/component/footernav/footernav.vue
+++ vue-project/src/component/footernav/footernav.vue
@@ -1,19 +1,19 @@
 <template>
         <mt-tabbar id='footer' v-model="selected" @click.native.stop="send" >
               <mt-tab-item id="tab1"  >
-                <img slot="icon" src="../../assets/img/icon1.png"  >
+                <img slot="icon" src="src/assets/img/icon1.png"  >
                 首页
               </mt-tab-item>
               <mt-tab-item id="tab2" >
-                <img slot="icon" src="../../assets/img/icon3.png">
+                <img slot="icon" src="src/assets/img/icon3.png">
                 优选
               </mt-tab-item>
               <mt-tab-item id="tab3"  >
-                <img slot="icon" src="../../assets/img/icon2.png">
+                <img slot="icon" src="src/assets/img/icon2.png">
                订单
               </mt-tab-item>
               <mt-tab-item id="tab4" >
-                <img slot="icon" src="../../assets/img/icon4.png">
+                <img slot="icon" src="src/assets/img/icon4.png">
                 我的
             </mt-tab-item>
         </mt-tabbar>
